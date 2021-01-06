ad1ee0d76231e70d316849c5db71e2dcb53b9762
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2021-01-06 11:05:57 -0800

    Modified to use wrapper class

--- ChuckNorris.py
+++ ChuckNorris.py
@@ -1,29 +1,82 @@
 # Author-Patrick Rainsberry
 # Description-Chuck Norris Jokes using the Requests Package
 
-import adsk.core, adsk.fusion, adsk.cam, traceback
-
 import os
 import sys
 
+import adsk.cam
+import adsk.core
+import adsk.fusion
+import traceback
 
-def remove_from_path(name):
-    if name in sys.path:
-        sys.path.remove(name)
-        remove_from_path(name)
+from contextlib import ContextDecorator
+from os.path import dirname, abspath
+
+APP_PATH = dirname(abspath(__file__))
 
 
-app_path = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(app_path, 'lib'))
-import requests
-remove_from_path(os.path.join(app_path, 'lib'))
+class lib_import(ContextDecorator):
+    """The lib_import class is a wrapper class to allow temporary import of a local library directory
+
+            By default it assumes there is a folder named 'lib' in the add-in root directory.
+
+            First install a 3rd party library (such as requests) to this directory.
+
+            .. code-block:: bash
+
+                # Assuming you are in the add-in root directory (sudo may not be required...)
+                sudo python3 -m pip install -t ./lib requests
+
+            Then you can temporarily import the library before making a call to the requests function.
+            To do this use the *@lib_import(...)* decorator on a function that uses the library.
+
+            Here is an example function for using `Requests <https://requests.readthedocs.io/en/master/>`_:
+
+            .. code-block:: python
+
+                @lib_import(APP_PATH)
+                def make_request(url, headers=None):
+                    import requests
+                    r = requests.get(url, headers=headers)
+                    return r
+
+            Args:
+                app_path(str): The root path of the addin.  Should be dynamically calculated.
+                library_folder(:obj:`str`, optional): Library folder name (relative to app root). Defaults to 'lib'
+            """
+
+    def __init__(self, app_path, library_folder='lib'):
+        super().__init__()
+        self.path = ''
+        self.app_path = app_path
+        self.library_folder = library_folder
+
+    def __enter__(self):
+        self.path = os.path.join(self.app_path, self.library_folder)
+        sys.path.insert(0, self.path)
+        return self
+
+    def __exit__(self, *exc):
+        if self.path in sys.path:
+            sys.path.remove(self.path)
+        return False
+
+
+@lib_import(APP_PATH)
+def make_request(url, headers=None):
+    import requests
+    r = requests.get(url, headers=headers)
+    return r
+
 
 def run(context):
     ui = None
     try:
         app = adsk.core.Application.get()
         ui = app.userInterface
-        r = requests.get('https://api.chucknorris.io/jokes/random')
+
+        r = make_request('https://api.chucknorris.io/jokes/random')
+
         r_json = r.json()
         joke = r_json['value']
         ui.messageBox(joke)
